d5d615c985cb732e2cecdd3106c7271fa215ff10
sfs-inep <igornepipenko@gmail.com>
2018-08-20 11:03:39 +0300

    initial

---
+++ lesson-1/lesson-1.js
@@ -0,0 +1,95 @@
+// console.log(a);
+// let a = 1;
+
+// for (let i = 0; i < 5; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     })
+// }
+
+// const a = { b: 1 };
+// a.c = 1;
+// const a = 1;
+// console.log(typeof a );
+// const b = undefined;
+// console.log(typeof b );
+// const c = null;
+// console.log(typeof c );
+
+// const a = new Date();
+// const b = /\d/;
+// console.log(typeof a );
+// console.log(typeof b );
+
+// let key1 = Symbol('key');
+// let key2 = Symbol('key');
+// console.log(key1 === key2);
+
+// let a = [2,2,3,4,3,5,4];
+// let b = new Set(a);
+// console.log(b);
+// console.log([...b]);
+
+// function a() {
+//     if(!true){
+//        if(){
+//            asdSda
+//            asddsa
+//        }
+//     }
+
+//     return 
+//     //code 
+// }
+
+// let a = {
+//     b: 1,
+//     c: 2
+// };
+// Object.defineProperty(a, 'c', {
+//     enumerable: false
+// })
+// for (key in a) {
+//     console.log(key);
+// }
+
+// z:for(let i = 0; i<10; i++){
+//     console.log(`i => ${i}`);
+//     for(let j = 0; j<10; j++){
+//         console.log(`j => ${j}`);
+//         if(i === 3){
+//             break z;
+//         }
+//     }
+// }
+//let a = Boolean(b);
+// let dc = [1, , 2].filter(Boolean)
+// console.log(dc)
+
+// let obj = { a: 1, b: 2, c: 4 };
+// // let bc, b, c, d;
+// // ({ a: 1, b, c, d = 1 }) = obj;
+// console.log(bc, b, c, d);
+
+
+// function a({a,b}){
+//     console.log(a,b)
+// }
+// a({a:1,b:2});
+
+// function b (c=1){
+//     console.log(c);
+// }
+//const a: ()=>{} = ()=>{}
+// const a = {a:4}
+// Object.defineProperty(a, 'c', {
+//     get: function () {
+//         return this.a * 2;
+//     }
+// })
+// console.log(a.c);
+// for (key in a) {
+//     console.log(key);
+// }
+// let a = 'sssass';
+// Array.prototype.reduce.apply(a)
