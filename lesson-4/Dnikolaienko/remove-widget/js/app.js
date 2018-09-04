5a1d707833ee3a02a8245c0e2b0f50771b45b0ea
Dima <dmitriy@MacBook-Pro-1.local>
2018-09-05 00:29:08 +0300

    Delete all homeworks

--- lesson-4/Dnikolaienko/remove-widget/js/app.js
+++
@@ -1,9 +0,0 @@
-let main = document.getElementById('main-container');
-
-main.addEventListener('click', (event) => {
-    if (!event.target.classList.contains('remove-btn')) { 
-        return;
-    }
-
-    event.target.parentNode.hidden = !event.target.parentNode.hidden;
-});
