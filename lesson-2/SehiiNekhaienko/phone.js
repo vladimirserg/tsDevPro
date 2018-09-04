dbb251c5a5b46b82d55ec8bd565cab472b1eee39
Serhii Nekhaienko <serhii.nekhaienko@dev-pro.net>
2018-09-04 13:20:19 +0300

    HW-3 Serhii Nekhaienko

--- lesson-2/SehiiNekhaienko/phone.js
+++
@@ -1,9 +0,0 @@
-function validatePhone(phone) {
-    let expression = /^(\()?([0-9]{3})(\))?(\-)?([0-9]{3})(\-)?([0-9]{4})$/;
-    return expression.test(phone);
-}
-
-console.log(validatePhone('(123)-123-1231')); // true
-console.log(validatePhone('2331231234')); // true
-console.log(validatePhone('123-123-1231')); // true
-console.log(validatePhone('+380(12)3-123-1231')); //false
