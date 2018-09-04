438bb3c7b12ca2694ba3745abde04b53ed437db6
Dmytro Nikolaienko <dmytro.nikolaienko@dev-pro.net>
2018-09-04 18:55:24 +0300

    test

--- lesson-4/Dnikolaienko/menu-widget/js/app.js
+++ lesson-4/Dnikolaienko/menu-widget/js/app.js
@@ -1,19 +1,37 @@
-(function(){
-    document.getElementsByTagName('ul')[0].onclick = function(event) {
-        let target = event.target;
-        let li = target.parentNode;
-        let childrenContainer = li.getElementsByTagName('ul')[0];
+
+ // document.getElementsByTagName('ul')[0].onclick = function(event) {
+    //     let target = event.target;
+    //     let li = target.parentNode;
+    //     let childrenContainer = li.getElementsByTagName('ul')[0];
     
     
-        if (!childrenContainer) {
-            return;
-        } 
+    //     if (!childrenContainer) {
+    //         return;
+    //     } 
     
-        if(!childrenContainer.classList.contains('hidden')){
-            hideList(childrenContainer);
+    //     if(!childrenContainer.classList.contains('hidden')){
+    //         hideList(childrenContainer);
+    //     }
+    // }
+
+    let list = document.querySelector('.list');
+
+    list.addEventListener('click', (e) => {
+        let chldrn = e.target.parentElement.children;
+        let children = e.target.parentNode.children;
+        
+        for (j = 0; j < children.length; j++) {
+            console.log(children[j]);
         }
-    }
-})();
+
+        // for(let i = 1; i<chldrn.length; i++) {
+        //     if(chldrn[i].tagName === "LI") {
+        //         return;
+        //     }
+        //     hideList(chldrn[i]);
+        // }
+    });
+
 
 function hideList(elem) {
     if(elem.classList.contains('hidden')) {
