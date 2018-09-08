cbbc460f6a007f5e4e39edfc0317f94690de69c6
Dima <dmitriy@MacBook-Pro-1.local>
2018-09-08 16:57:29 +0300

    HW2 - use eslint and add comment

--- lesson-2/Dnikolaienko/app.js
+++ lesson-2/Dnikolaienko/app.js
@@ -1,34 +1,34 @@
 function testReg(reg, str) {
-    let answer;
-    if (reg.test(str)) {
-      answer = 'подходит под';
-    } else {
-      answer = 'не подходит под';
-    }
-    console.log(`${str} ${answer} ${reg.source}`);
+  let answer; // инициализируем ответ
+  if (reg.test(str)) { // проверяем входящую строку, на соответсвие входящему рег. выражению
+    answer = 'подходит под';
+  } else {
+    answer = 'не подходит под';
+  }
+  console.log(`${str} ${answer} ${reg.source}`);
 }
 
 // Telephone
-let phoneReg = /\(?\d{3}\)?-?\d{3}-?\d{4}/g;
-let phoneStr = '(123)-222-2222';
+const phoneReg = /\(?\d{3}\)?-?\d{3}-?\d{4}/g; // рег. выражение для телефона
+const phoneStr = '(123)-222-2222';
 testReg(phoneReg, phoneStr);
 
 // URL
-let domainReg = /^(?:https?:\/\/)?\w+(?:\.\w+)?(?:\.[A-Z]{2,3})+$/i;
-let domain = 'https://thesaurus.net';
+const domainReg = /^(?:https?:\/\/)?\w+(?:\.\w+)?(?:\.[A-Z]{2,3})+$/i; // рег. выражение для домена
+const domain = 'https://thesaurus.net';
 testReg(domainReg, domain);
 
 // Twitter or Telegram
-let nickNameReg = /^@?(\w+)$/;
-let nickName = '@Dimas2908';
+const nickNameReg = /^@?(\w+)$/; // рег.выражение для пользователя телеграма или твитера
+const nickName = '@Dimas2908';
 testReg(nickNameReg, nickName);
 
 // Email
-let emailReg = /[^\s@]+@[^\s@.]+\.[^\s@]+/;
-let email = 'yodimec2908@mail.com.ua';
+const emailReg = /[^\s@]+@[^\s@.]+\.[^\s@]+/; // рег. выражение для электронной почты
+const email = 'yodimec2908@mail.com.ua';
 testReg(emailReg, email);
 
 // Name Lastname
-let personReg = /^(\S+) (\S*) ?\b(\S+)$/;
-let personData = 'Nikolaienko Dmytro Mykolayovich';
+const personReg = /^(\S+) (\S*) ?\b(\S+)$/; // рег. выражение для персональных данных
+const personData = 'Nikolaienko Dmytro Mykolayovich';
 testReg(personReg, personData);
